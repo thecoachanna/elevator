2e316c1712620b04fa0c07b27c223bdef030b5cd
Anna Nettles <99093454+thecoachanna@users.noreply.github.com>
2022-12-16 10:12:20 -0500

    level 2 if/else

--- index.py
+++ index.py
@@ -21,16 +21,28 @@ while True:
         print_pause("After a few moments, you find "
                     "yourself in the lobby.")
             if 'ID card' in items:
-                print_sleep("The clerk greets you, but she has already "
+                print_pause("The clerk greets you, but she has already "
                         "given you your ID card, so there is nothing "
                         "more to do here now.")
             else:
-                print_sleep("The clerk greets you and gives you your ID card.")
+                print_pause("The clerk greets you and gives you your ID card.")
                 items.append('ID card')
     elif floor == '2':
         print_pause("You push the button for the second floor.")
         print_pause("After a few moments, you find yourself "
                     "in the human resources department.")
+                    if 'handbook' in items:
+                        print_pause("The HR folks are busy at their desks.")
+                        print_pause("There doesn't seem to be much to do here.")
+                    else:
+                        print_pause("The head of HR greets you.")
+                            if 'ID card' in items:
+                                print_pause("He looks at your ID card and then gives you a copy of the employee handbook.")
+                                items.append('handbook')
+                            else:
+                                print_pause("He has something for you, but says he can't "
+                                    "give it to you until you go get your ID card.")
+                    print_pause("You head back to the elevator.")  
     elif floor == '3':
         print_pause("You push the button for the third floor.")
         print_pause("After a few moments, you find yourself "
